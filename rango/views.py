81864b3866364637b4ffba05ce3c3068c560eb4a
Iain Macdonald <iain.macdonald@williamhill.com>
2017-01-24 09:36:01 +0000

    Updated from chapter 4

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse("Rango Says hey there Partner! </br> <a href='/rango/about/'>about</a>")
+    context_dict = {'boldmessage': "Crunchy,creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return HttpResponse("Rango Says this is the about page! </br> <a href='/rango/'>index</a>")
-
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,9 +12,16 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+print (__file__)
+print(os.path.dirname(__file__))
+print(os.path.dirname(os.path.dirname(__file__)))
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+MEDIA_DIR = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = MEDIA_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -27,6 +34,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+STATICFILES_DIRS = [ STATIC_DIR,]
 
 # Application definition
 
@@ -55,7 +64,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
