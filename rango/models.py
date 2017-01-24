68fe6b58f9937a04578bfdcadb62f55a3cdde831
Iain Macdonald <iain.macdonald@williamhill.com>
2017-01-24 22:36:50 +0000

    Chapter 5 snaphot of data models

--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=128,unique=True)
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
 
     def __str__(self):
-        return self.name
-
-    def __unicode__(self):
         return self.name
 
 class Page(models.Model):
@@ -16,7 +16,4 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.name
-
-    def __unicode__(self):
-        return self.name
+        return self.title
