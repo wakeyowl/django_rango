121506c8b4a66c20763f49a142899024c90aa1f3
Iain Macdonald <iain.macdonald@williamhill.com>
2017-01-30 10:57:11 +0000

    Updated from chapter 4

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from rango import views
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
-    url(r'^about/',views.about, name='about'),
+    url(r'^$', views.index, name='index'),
+    url(r'about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
 ]
