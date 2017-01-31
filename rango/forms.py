a35ac45089d2e2f1d85c2917a0e503fe5ed6f409
Iain Macdonald <iain.macdonald@williamhill.com>
2017-01-31 12:58:06 +0000

    Updated upto Chapter 7

--- rango/forms.py
+++ rango/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from rango.models import Page,Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,help_text="Please Enter the category Name.")
+    name = forms.CharField(max_length=128, help_text="Please Enter the category Name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -12,9 +12,19 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,help_text="Please enter teh title on the page.")
-    url = forms.URLField(max_length=200,help_text="Please enter the URL of the page")
-    views = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+    title = forms.CharField(max_length=128, help_text="Please enter the title on the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+            return cleaned_data
 
     class Meta:
         model = Page
