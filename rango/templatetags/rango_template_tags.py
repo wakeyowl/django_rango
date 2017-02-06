6003723d91ff61dfed01d0d777a3c118c5a7fcd1
Iain Macdonald <iain.macdonald@williamhill.com>
2017-02-06 07:32:08 +0000

    Updated everyting upto 9.7 section of the book

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -3,6 +3,8 @@ from rango.models import Category
 
 register = template.Library()
 
+
+# template Tags
 @register.inclusion_tag('rango/cats.html')
 def get_category_list(cat=None):
     return {'cats': Category.objects.all(),
