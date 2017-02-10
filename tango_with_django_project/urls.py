0acf82fd302910e53796b796c5c622ea22a9c2cf
Iain Macdonald <iain.macdonald@williamhill.com>
2017-02-10 07:05:33 +0000

    Updating bootstrap details
    Not working still
    - About
    - Register
    - Login

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,11 +18,21 @@ from django.contrib import admin
 from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
+from registration.backends.simple.views import RegistrationView
+
 
 urlpatterns = [
                   url(r'^$', views.index, name='index'),
                   url(r'^rango/', include('rango.urls')),
                   url(r'^admin/', admin.site.urls),
                   url(r'^accounts/', include('registration.backends.simple.urls')),
+                  url(r'^accounts/register/$',
+                      RegistrationView.as_view(),
+                      name='registration_register'),
               ] + static(settings.MEDIA_URL,
                          document_root=settings.MEDIA_ROOT)
+
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
