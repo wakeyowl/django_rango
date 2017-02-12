c317debae915f99d15e6d95276b9d85310ed86fa
Iain Macdonald <iain.macdonald@williamhill.com>
2017-02-12 12:43:48 +0000

    Updated all forms to use the inbuilt bootstrap app..
    {% load bootstrap_toolkit %}
    
    Still got issues on login ..
    
    NoReverseMatch at /rango/
    Reverse for 'profile' with arguments '('imacdonald',)' and keyword arguments '{}' not found. 0 pattern(s) tried: []

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,18 +21,18 @@ from django.conf.urls.static import static
 from registration.backends.simple.views import RegistrationView
 
 
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
+
 urlpatterns = [
                   url(r'^$', views.index, name='index'),
                   url(r'^rango/', include('rango.urls')),
                   url(r'^admin/', admin.site.urls),
                   url(r'^accounts/', include('registration.backends.simple.urls')),
-                  url(r'^accounts/register/$',
-                      RegistrationView.as_view(),
-                      name='registration_register'),
+                  url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
               ] + static(settings.MEDIA_URL,
                          document_root=settings.MEDIA_ROOT)
 
 
-class MyRegistrationView(RegistrationView):
-    def get_success_url(self, user):
-        return '/rango/'
+
